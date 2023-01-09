69fe8dd4342f51a1a0ba4ff97b06de04c26c40df
Nikolay <m.nikolay1990@gmail.com>
2023-01-09 13:08:56 +0700

    refactoring

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,13 +1,14 @@
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
 import { TLayoutProps } from './Layout.props';
+import styles from './Layout.module.scss';
 
 export default function Layout({ children }: TLayoutProps) {
 	return (
-		<>
+		<div className={styles.wrapper}>
 			<Header />
 			{children}
 			<Footer />
-		</>
+		</div>
 	);
 }
