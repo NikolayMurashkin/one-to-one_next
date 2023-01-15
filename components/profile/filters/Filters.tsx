f8820f6f41f74ab536db82fb8019906a224f5b50
Nikolay <m.nikolay1990@gmail.com>
2023-01-15 11:50:43 +0700

    added sort

--- components/profile/filters/Filters.tsx
+++ components/profile/filters/Filters.tsx
@@ -1,18 +1,9 @@
 import styles from './Filters.module.scss';
-import Tab from '../../ui/tabs/Tabs';
 import Button from '../../ui/button/Button';
 
 const Filters = () => {
 	return (
 		<section className={styles.wrapper}>
-			<div className={styles.tabs}>
-				<Tab label='Собеседование' />
-				<Tab label='Фильтр 1' />
-				<Tab label='Фильтр 2' />
-				<Tab label='Фильтр 3' />
-				<Tab label='Фильтр 4' />
-				<Tab label='Фильтр 5' />
-			</div>
 			<div className={styles.interview}>
 				<span>1 СОБЕСЕДОВАНИЕ (НОВОЕ, ЕЩЕ НЕ ПРОВЕДЕННОЕ)</span>
 				<div className={styles.buttons}>
