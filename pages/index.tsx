702c5eac38aac8ab7dac106691e922edbaf58313
Nikolay <m.nikolay1990@gmail.com>
2023-01-19 14:27:12 +0700

    try to connect to server api

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,15 +16,15 @@ const HomePage: React.FC<TIndexProps> = ({ interviews, questions }) => {
 	const [questionItems, setQquestionItems] = useState();
 
 	const { data, error } = useSWR(
-		'http://51.250.8.47:8080/one-to-one/api/v1/one-to-one?search=status:OPEN',
+		'http://localhost:8080/one-to-one/api/v1/one-to-one?search=status:OPEN',
 		fetcher
 	);
 
 	useEffect(() => {
-		fetch('http://51.250.8.47:8080/one-to-one/api/v1/one-to-one')
+		fetch('http://localhost:8080/one-to-one/api/v1/one-to-one')
 			.then((res) => res.json())
 			.then((data) => setInterviewItems(data));
-		fetch('http://51.250.8.47:8080/one-to-one/api/v1/user/1/question')
+		fetch('http://localhost:8080/one-to-one/api/v1/user/1/question')
 			.then((res) => res.json())
 			.then((data) => setQquestionItems(data));
 	}, []);
