d2864d6f7d0a98fe0d3803b600aad3608833f549
Nikolay <m.nikolay1990@gmail.com>
2023-02-20 16:52:07 +0700

    set redirect if not logged

--- shared/ui/layout/Layout.tsx
+++ shared/ui/layout/Layout.tsx
@@ -8,21 +8,23 @@ import { useGetUserQuery } from '../header/api/getUserApiSlice';
 
 export default function Layout({ children }: TLayoutProps) {
 	const [user, setUser] = useState<number>();
+
+	const router = useRouter();
+	const isAuthPage =
+		router.pathname === '/login' || router.pathname === '/register';
+
 	useEffect(() => {
 		const userIdJson = localStorage.getItem('id');
 		if (userIdJson) {
 			setUser(userIdJson !== null ? JSON.parse(userIdJson) : {});
 		}
-		if (localStorage.getItem('id') === null) {
+		if (isAuthPage && localStorage.getItem('id') === null) {
 			router.push('/login');
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	const { data: userData, isLoading } = useGetUserQuery(user);
-	const router = useRouter();
-	const isAuthPage =
-		router.pathname === '/login' || router.pathname === '/register';
 	if (isLoading) {
 		return <p>Загрузка...</p>;
 	}
