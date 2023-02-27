26974336ab3c6d3a8450f457c7b2728634390f45
Nikolay <m.nikolay1990@gmail.com>
2023-02-27 20:16:15 +0700

    added feedback page

--- shared/api/apiSlice.ts
+++ shared/api/apiSlice.ts
@@ -9,7 +9,7 @@ import { setCredentials, logOut } from '@features/auth/api/authSlice';
 import { RootState } from '@app/store';
 
 const baseQuery = fetchBaseQuery({
-	baseUrl: 'http://localhost:8080/one-to-one/api/v1',
+	baseUrl: 'http://55.250.51.231:8080/one-to-one/api/v1',
 	credentials: 'include',
 	prepareHeaders: (headers, { getState }) => {
 		const token = (getState() as RootState).auth.token;
