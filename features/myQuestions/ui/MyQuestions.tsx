5648cd90e87e6763f8829b2acadb4480f1881794
NikolayMurashkin <admin@MacBook-Pro-Admin.local>
2023-03-15 22:57:49 +0700

    try to infinite fetch questions with swr

--- features/myQuestions/ui/MyQuestions.tsx
+++ features/myQuestions/ui/MyQuestions.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import { skipToken } from '@reduxjs/toolkit/dist/query';
+import useSWRInfinite from 'swr/infinite';
+import { Fetcher } from 'swr';
 
 import styles from '../model/MyQuestions.module.scss';
 import { QuestionItem } from '@entities/questionItem';
 import { useGetMyQuestionsQuery } from '../api/getMyQustionsApiSlice';
-import { skipToken } from '@reduxjs/toolkit/dist/query';
+import { IGetMyQuestionResponse } from '../model/myQuestionsSliceTypes';
 
 export const MyQuestions = () => {
 	const [userId, setUserId] = useState<number>();
@@ -18,60 +20,57 @@ export const MyQuestions = () => {
 		}
 	}, []);
 
+	const getKey = (
+		pageIndex: number,
+		previousPageData: IGetMyQuestionResponse
+	) => {
+		if (previousPageData && !previousPageData.totalItems) return null; // reached the end
+		return `/user/${userId}/question?search=userId:${userId}&page=${pageIndex}&size=30`; // SWR key
+	};
+
+	const fetcher: Fetcher<IGetMyQuestionResponse, string> = async () => {
+		const res = await fetch(
+			`http://51.250.55.231:8080/one-to-one/api/v1`
+		).then((res) => res.json());
+		const data = await res.items;
+		return data;
+	};
+
+	const { data, error, isLoading, isValidating, mutate, size, setSize } =
+		useSWRInfinite(getKey, fetcher, { initialSize: 1 });
+
+	console.log(data);
+
 	//TODO: написать фетч для получения следующей страницы вопросов при скролле
 
-	const { data, error } = useGetMyQuestionsQuery( {userId, page} ?? skipToken);
+	// const { data, error } = useGetMyQuestionsQuery(
+	// 	{ userId, page } ?? skipToken
+	// );
 
-	if (!data) {
-		return <p>Загрузка...</p>;
-	}
-	if (data.totalItems == 0) {
-		return <p>Вы пока не добавили ни одного вопроса</p>;
-	}
+	// if (!data) {
+	// 	return <p>Загрузка...</p>;
+	// }
+	// if (data.totalItems == 0) {
+	// 	return <p>Вы пока не добавили ни одного вопроса</p>;
+	// }
 
-	if (error) {
-		return <p>Что-то пошло не так! Мы скоро всё исправим!</p>;
-	}
+	// if (error) {
+	// 	return <p>Что-то пошло не так! Мы скоро всё исправим!</p>;
+	// }
 
 	return (
 		<ul className={styles.list}>
-			<InfiniteScroll
-				dataLength={items.length} //This is important field to render the next data
-				next={fetchData}
-				hasMore={true}
-				loader={<h4>Loading...</h4>}
-				endMessage={
-					<p style={{ textAlign: 'center' }}>
-						<b>Yay! You have seen it all</b>
-					</p>
-				}
-				// below props only if you need pull down functionality
-				refreshFunction={this.refresh}
-				pullDownToRefresh
-				pullDownToRefreshThreshold={50}
-				pullDownToRefreshContent={
-					<h3 style={{ textAlign: 'center' }}>
-						&#8595; Pull down to refresh
-					</h3>
-				}
-				releaseToRefreshContent={
-					<h3 style={{ textAlign: 'center' }}>
-						&#8593; Release to refresh
-					</h3>
-				}
-			>
-				{data &&
-					data.items.map((item) => {
-						return (
-							<QuestionItem
-								key={item.id}
-								answer={item.answer}
-								question={item.question}
-								technology={item.technology?.name}
-							/>
-						);
-					})}
-			</InfiniteScroll>
+			{/* {data &&
+				data.items.map((item) => {
+					return (
+						<QuestionItem
+							key={item.id}
+							answer={item.answer}
+							question={item.question}
+							technology={item.technology?.name}
+						/>
+					);
+				})} */}
 		</ul>
 	);
 };
