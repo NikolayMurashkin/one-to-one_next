cc87b2372c88dd6d17e864bd1ca0fed65492e36d
NikolayMurashkin <admin@MacBook-Pro-Admin.local>
2023-03-16 15:09:56 +0700

    try to set btn available when 5 minutes left to interview

--- entities/myInterviewItem/ui/InterviewItem.tsx
+++ entities/myInterviewItem/ui/InterviewItem.tsx
@@ -55,11 +55,11 @@ export const InterviewItem: React.FC<IInterviewItemProps> = ({
 		router.push('/feedback');
 	};
 
-	const timeBeforeInterview = new Date(interviewDate).getMilliseconds();
+	const timeBeforeInterview = Date.parse(interviewDate);
 	const fiveMinutesBeforeInterview =
 		new Date(interviewDate).getMilliseconds() - 300000;
 	console.log(timeBeforeInterview);
-	console.log(fiveMinutesBeforeInterview);
+	// console.log(fiveMinutesBeforeInterview);
 
 	return (
 		<li
