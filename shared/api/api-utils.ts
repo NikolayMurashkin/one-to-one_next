ea3f01a49ae26d71b075d53060930633c038cdd1
Nikolay <m.nikolay1990@gmail.com>
2023-03-22 17:36:21 +0700

    try to set users locale timezone for time in 1-1

--- shared/api/api-utils.ts
+++ shared/api/api-utils.ts
@@ -1,13 +1,13 @@
 import { IInterviewItem } from '@features/myInterviews/model/interviewItemSliceTypes';
 
-
 interface Options {
 	year: 'numeric';
 	month: 'long';
 	day: 'numeric';
 	hour: '2-digit';
 	minute: '2-digit';
-};
+	timeZone: string;
+}
 
 export const getDate = (data: IInterviewItem, newDate: string): string => {
 	if (data) {
@@ -17,9 +17,10 @@ export const getDate = (data: IInterviewItem, newDate: string): string => {
 			day: 'numeric',
 			hour: '2-digit',
 			minute: '2-digit',
+			timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
 		};
-		const userLocale = Intl.DateTimeFormat().resolvedOptions().timeZone;
-		const date = new Date(newDate).toLocaleDateString(userLocale, options);
+		// const userLocale = Intl.DateTimeFormat().resolvedOptions().timeZone;
+		const date = new Date(newDate).toLocaleDateString('ru-RU', options);
 		return `${date} MSK`;
 	} else {
 		return '';
