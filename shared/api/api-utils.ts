94a459febc1b325e2842b3317e0795e58216902a
NikolayMurashkin <admin@MacBook-Pro-Admin.local>
2023-03-23 23:57:12 +0700

    try to set time to UTC for create interview

--- shared/api/api-utils.ts
+++ shared/api/api-utils.ts
@@ -12,38 +12,62 @@ interface timeOptions {
 	timeZone: string;
 }
 
+interface dateTimeOptions {
+	year: 'numeric';
+	month: 'long';
+	day: 'numeric';
+	hour: '2-digit';
+	minute: '2-digit';
+	timeZone: string;
+}
+
 export const getDate = (data: IInterviewItem, newDate: string): string => {
 	if (data) {
 		const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 		// console.log(userTimeZone);
 
-		const dateOptions: dateOptions = {
+		// const dateOptions: dateOptions = {
+		// 	year: 'numeric',
+		// 	month: 'long',
+		// 	day: 'numeric',
+		// 	timeZone: userTimeZone,
+		// };
+		// const timeOptions: timeOptions = {
+		// 	hour: '2-digit',
+		// 	minute: '2-digit',
+		// 	// timeZone: 'Pacific/Samoa',
+		// 	// timeZone: 'Asia/Krasnoyarsk',
+		// 	timeZone: userTimeZone,
+		// };
+
+
+		const dateTimeOptions: dateTimeOptions = {
 			year: 'numeric',
 			month: 'long',
 			day: 'numeric',
-			timeZone: userTimeZone,
-		};
-		const timeOptions: timeOptions = {
 			hour: '2-digit',
 			minute: '2-digit',
-			// timeZone: 'Pacific/Samoa',
-			// timeZone: 'Asia/Krasnoyarsk',
 			timeZone: userTimeZone,
-		};
+
+		}
 		// const day = new Date(newDate).toLocaleDateString('ru-RU', dateOptions);
 		// const time = new Date(newDate).toLocaleTimeString('ru-RU', timeOptions);
-		const day = new Date(newDate).toLocaleDateString('ru-RU', dateOptions);
-		const time = new Date(newDate).toLocaleTimeString('ru-RU', timeOptions);
-		const fullDate = new Date(`${day} ${time}`).toLocaleDateString(
+		// const day = new Date(newDate).toLocaleDateString('ru-RU', dateOptions);
+		// const time = new Date(newDate).toLocaleTimeString('ru-RU', timeOptions);
+		// const fullDate = new Date(`${day} ${time}`).toLocaleDateString(
+		// 	'ru-RU',
+		// 	{ timeZone: 'Asia/Krasnoyarsk' }
+		// );
+		const date = new Date(newDate).toLocaleDateString(
 			'ru-RU',
-			{ timeZone: 'Asia/Krasnoyarsk' }
+			dateTimeOptions
 		);
-		console.log(day);
-		console.log(time);
-		console.log(fullDate);
+		console.log(date);
+		// console.log(time);
+		// console.log(fullDate);
 		// const date = new Date(newDate).toLocaleDateString('ru-RU', options);
-		const dateTime = `${day} в ${time}`;
-		return `${fullDate}`;
+		// const dateTime = `${day} в ${time}`;
+		return `${date}`;
 	} else {
 		return '';
 	}
