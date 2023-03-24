7ec5cacab7d35b921722bf796e2498dbf57211b2
Nikolay <m.nikolay1990@gmail.com>
2023-03-24 22:01:51 +0700

    оптимизировал функцию getDate

--- shared/api/api-utils.ts
+++ shared/api/api-utils.ts
@@ -1,33 +1,16 @@
 import { IInterviewItem } from '@features/myInterviews/model/interviewItemSliceTypes';
 
-interface dateOptions {
-	year: 'numeric';
-	month: 'long';
-	day: 'numeric';
-	timeZone: string;
-}
-interface timeOptions {
-	hour: '2-digit';
-	minute: '2-digit';
-	timeZone: string;
-}
-
-interface dateTimeOptions {
-	year: 'numeric';
-	month: 'long';
-	day: 'numeric';
-	hour: '2-digit';
-	minute: '2-digit';
-	timeZone: string;
-}
-
-export const getDate = (data: IInterviewItem, newDate: string): string => {
-	if (!data) {
+export const getDate = (
+	data: IInterviewItem | undefined,
+	newDate: string
+): string => {
+	if (!data || !newDate) {
 		return '';
 	}
 
 	const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-	const dateTimeOptions: dateTimeOptions = {
+
+	const dateTimeOptions: Intl.DateTimeFormatOptions = {
 		year: 'numeric',
 		month: 'long',
 		day: 'numeric',
@@ -36,15 +19,15 @@ export const getDate = (data: IInterviewItem, newDate: string): string => {
 		timeZone: userTimeZone,
 	};
 
-	const recievedDateTime = new Date(newDate).getTime();
-	const recievedDateOffset = Math.abs(
-		new Date(newDate).getTimezoneOffset() * 60 * 1000
-	);
-	const dateTime = new Date(
-		recievedDateOffset + recievedDateTime
-	).toLocaleString('ru-RU', dateTimeOptions);
+	try {
+		const date = new Date(`${newDate}Z`);
+		const dateTime = date.toLocaleString('ru-RU', dateTimeOptions);
 
-	return `${dateTime}`;
+		return dateTime;
+	} catch (error) {
+		console.error(error);
+		return '';
+	}
 };
 
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
